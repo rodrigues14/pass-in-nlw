f2160211dfdb422133f953d15415ad357fa01ea2
Lucas Rodrigues <lukinhasr1423@gmail.com>
2024-04-02 14:53:32 -0300

    Criado as migrations e a estrutura do projeto

---
+++ src/main/java/com/rocketseat/passin/PassinApplication.java
@@ -0,0 +1,13 @@
+package com.rocketseat.passin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketseat/passin/PassinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.passin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
